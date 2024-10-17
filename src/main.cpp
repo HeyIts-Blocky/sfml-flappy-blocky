518f7c1d4d912da651304a20d1d04a42f790f437
Blocky <101740836+HeyIts-Blocky@users.noreply.github.com>
2024-10-17 14:00:57 -0600

    balls

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,35 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <filesystem>
+#include <chrono>
 
 // methods
-void renderLoop(sf::RenderWindow &window);
+void renderLoop(sf::RenderWindow &window, sf::View &view);
+void updateLoop();
+sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight);
 //
+
+long deltaTime = 1;
+long lastTime = -1;
+
+sf::Sprite bgSprite;
 
 int main()
 {
-
-    sf::RenderWindow window({700u, 500u}, "Balls.");
+    unsigned int resX = 700, resY = 500;
+    sf::RenderWindow window({resX, resY}, "Flappy Blocky");
     window.setFramerateLimit(144);
+
+    sf::View view;
+    view.setSize( resX, resY );
+    view.setCenter( view.getSize().x / 2, view.getSize().y / 2 );
+    view = getLetterboxView( view, resX, resY ); 
+
+    // Load textures
+    sf::Texture bgTex;
+    if(!bgTex.loadFromFile("data/images/ryo.png")) return 1;
+    bgSprite = sf::Sprite(bgTex);
+    bgSprite.setScale(sf::Vector2f(3.f, 2.f));
 
     while (window.isOpen())
     {
@@ -20,47 +39,88 @@ int main()
             {
                 window.close();
             }
+
+            if (event.type == sf::Event::Resized){
+                unsigned int width = event.size.width;
+                unsigned int height = event.size.height;
+                if(event.size.width < resX){
+                    window.setSize(sf::Vector2u(resX, height));
+                    width = resX;
+                }
+                if(event.size.height < resY){
+                    window.setSize(sf::Vector2u(width, resY));
+                    height = resY;
+                }
+                view = getLetterboxView( view, width, height);
+            }
         }
 
-        renderLoop(window);
+        updateLoop();
+        renderLoop(window, view);
 
     }
 }
 
-void render(sf::RenderTexture &render){
+void render(sf::RenderWindow &window){
     
-    // sf::CircleShape circol(50.f);
-
-    // circol.setFillColor(sf::Color(0, 255, 0));
-    // circol.setOutlineThickness(3.f);
-    // circol.setOutlineColor(sf::Color::White);
-    // render.draw(circol);
-
-    sf::Texture texture;
-    if(!texture.loadFromFile("data/images/ryo.png")){
-        return;
-    }
-    sf::Sprite sprite(texture);
-    sprite.setPosition(sf::Vector2f(30.f, 50.f));
-    sprite.setRotation(50.f);
-    render.draw(sprite);
-
+    window.draw(bgSprite);
 
 }
-void renderLoop(sf::RenderWindow &window){
-    sf::RenderTexture tex;
-    if(!tex.create(700, 500)){
-        exit(3);
-    }
-
-    tex.clear();
-    render(tex);
-    tex.display();
-    
+void renderLoop(sf::RenderWindow &window, sf::View &view){
     window.clear();
 
-    sf::Sprite texSprite(tex.getTexture());
-    window.draw(texSprite);
+    window.setView(view);
+    render(window);
 
     window.display();
+}
+
+void update(){
+    
+    
+
+}
+void updateLoop(){
+    // Calculate deltaTime
+    long now = std::chrono::system_clock::now().time_since_epoch() / std::chrono::milliseconds(1);
+    deltaTime = lastTime != -1 ? now - lastTime : 0;
+    lastTime = now;
+    
+    update();
+}
+
+
+sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight) {
+
+    // Compares the aspect ratio of the window to the aspect ratio of the view,
+    // and sets the view's viewport accordingly in order to achieve a letterbox effect.
+    // A new view (with a new viewport set) is returned.
+
+    float windowRatio = (float) windowWidth / (float) windowHeight;
+    float viewRatio = view.getSize().x / (float) view.getSize().y;
+    float sizeX = 1;
+    float sizeY = 1;
+    float posX = 0;
+    float posY = 0;
+
+    bool horizontalSpacing = true;
+    if (windowRatio < viewRatio)
+        horizontalSpacing = false;
+
+    // If horizontalSpacing is true, the black bars will appear on the left and right side.
+    // Otherwise, the black bars will appear on the top and bottom.
+
+    if (horizontalSpacing) {
+        sizeX = viewRatio / windowRatio;
+        posX = (1 - sizeX) / 2.f;
+    }
+
+    else {
+        sizeY = windowRatio / viewRatio;
+        posY = (1 - sizeY) / 2.f;
+    }
+
+    view.setViewport( sf::FloatRect(posX, posY, sizeX, sizeY) );
+
+    return view;
 }
