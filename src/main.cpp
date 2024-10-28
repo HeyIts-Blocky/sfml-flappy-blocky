a9ec02f1c1f00fd4045722115c1e1bf6c2fb92e9
Blocky <101740836+HeyIts-Blocky@users.noreply.github.com>
2024-10-27 19:19:41 -0600

    yeehaw or sumn idk

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,32 @@
 #include <iostream>
 #include <filesystem>
 #include <chrono>
+#include <deque>
 
 // methods
 void renderLoop(sf::RenderWindow &window, sf::View &view);
 void updateLoop();
 sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight);
+void resizeSprite(sf::Sprite &sprite, sf::Vector2f size);
 //
+
+const unsigned int resX = 320, resY = 180;
 
 long deltaTime = 1;
 long lastTime = -1;
 
 sf::Sprite bgSprite;
+sf::Sprite plrSprite;
+
+float momentum = 0;
+bool clicking = false, fall = false;
+
+std::deque<sf::Sprite> walls;
 
 int main()
 {
-    unsigned int resX = 700, resY = 500;
-    sf::RenderWindow window({resX, resY}, "Flappy Blocky");
+    
+    sf::RenderWindow window({resX * 3, resY * 3}, "Flappy Blocky");
     window.setFramerateLimit(144);
 
     sf::View view;
@@ -27,9 +37,15 @@ int main()
 
     // Load textures
     sf::Texture bgTex;
-    if(!bgTex.loadFromFile("data/images/ryo.png")) return 1;
+    if(!bgTex.loadFromFile("data/images/balls 2.jpg")) return 1;
     bgSprite = sf::Sprite(bgTex);
-    bgSprite.setScale(sf::Vector2f(3.f, 2.f));
+    resizeSprite(bgSprite, sf::Vector2f(resX, resY));
+    sf::Texture plrTex;
+    if(!plrTex.loadFromFile("data/images/plr.png")) return 1;
+    plrSprite = sf::Sprite(plrTex);
+    resizeSprite(plrSprite, sf::Vector2f(32, 32));
+    plrSprite.setPosition(30, 100);
+    plrSprite.setOrigin(sf::Vector2f(16.f, 16.f));
 
     while (window.isOpen())
     {
@@ -61,23 +77,69 @@ int main()
     }
 }
 
-void render(sf::RenderWindow &window){
+void render(sf::RenderTexture &rend){
     
-    window.draw(bgSprite);
+    rend.draw(bgSprite);
+    rend.draw(plrSprite);
 
 }
 void renderLoop(sf::RenderWindow &window, sf::View &view){
     window.clear();
 
     window.setView(view);
-    render(window);
+    sf::RenderTexture rend;
+    if(!rend.create(resX, resY)){
+        return;
+    }
+    render(rend);
+    sf::Sprite rendSprite(rend.getTexture());
+    resizeSprite(rendSprite, sf::Vector2f(resX, resY));
+    rendSprite.setScale(rendSprite.getScale().x, rendSprite.getScale().y * -1);
+    rendSprite.move(0, resY);
+    window.draw(rendSprite);
 
     window.display();
 }
 
 void update(){
+
+    for(sf::Sprite sprite: walls){
+        sprite.move(sf::Vector2f(-0.1f * deltaTime, 0));
+    }
+    bool loop = true;
+    while(loop){
+        loop = false;
+        
+    }
     
-    
+    momentum += (0.005f * deltaTime);
+    if(momentum > 1.5f) momentum = 1.5f;
+
+    if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+        if(!clicking){
+            clicking = true;
+            momentum = -1.5f;
+            fall = true;
+        }
+    }else{
+        clicking = false;
+    }
+
+    if(fall){
+        plrSprite.move(0, momentum);
+        float percent = (momentum / 1.5f);
+        plrSprite.setRotation(30 * percent);
+
+        if(plrSprite.getPosition().y < 0){
+            plrSprite.setPosition(30, 0);
+            momentum = 0;
+        } 
+        if(plrSprite.getPosition().y > resY){
+            // death o.o
+        }
+    }else{
+        plrSprite.setRotation(0);
+    }
 
 }
 void updateLoop(){
@@ -123,4 +185,13 @@ sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight) {
     view.setViewport( sf::FloatRect(posX, posY, sizeX, sizeY) );
 
     return view;
+}
+
+void resizeSprite(sf::Sprite &sprite, sf::Vector2f size){
+    sf::Texture texture = *(sprite.getTexture());
+    sf::Vector2u texSize = texture.getSize();
+
+    float scaleX = size.x / (float)texSize.x;
+    float scaleY = size.y / (float)texSize.y;
+    sprite.setScale(sf::Vector2f(scaleX, scaleY));
 }
